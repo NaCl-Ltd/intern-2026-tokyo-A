03a9f048ef87c65a405a3b5bce0b729658796212
Daichi Otsuka <m23025@g.metro-cit.ac.jp>
2026-09-02 10:19:56 +0900

    edit files to add description

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,7 +64,7 @@ class UsersController < ApplicationController
 
     def user_params
       params.expect(user: [:name, :email, :password,
-                           :password_confirmation])
+                           :password_confirmation, :description])
     end
 
     # beforeフィルター
